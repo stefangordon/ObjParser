778e78874d97d888f7f8d2561f52b1cb7fb7b4a7
Josh Perry <joshperry@outlook.com>
2016-06-30 21:46:21 +0100

    Added some NUnit tests

---
+++ ObjParser_Tests/ProcessLine_Tests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using NUnit.Framework;
+using ObjParser;
+
+namespace ObjParser_Tests
+{
+    [TestFixture]
+    public class ProcessLine_Tests
+    {
+        private Obj obj;
+
+        [SetUp]
+        public void SetUp()
+        {
+            obj = new Obj();
+        }
+
+        #region Vertex
+        [Test]
+        public void ProcessLine_SingleVert_ValidInput()
+        {
+            // Arrange
+            var objFile = new[]
+            {
+                "v 0.0 0.0 0.0"
+            };
+
+            // Act
+            obj.LoadObj(objFile);
+
+            // Assert
+            Assert.IsTrue(obj.VertexList.Count == 1);
+        }
+
+        [Test]
+        public void ProcessLine_TwoVert_ValidInput()
+        {
+            // Arrange
+            var objFile = new[]
+            {
+                "v 0.0 0.0 0.0",
+                "v 1.0 1.0 1.0"
+            };
+
+            // Act
+            obj.LoadObj(objFile);
+
+            // Assert
+            Assert.IsTrue(obj.VertexList.Count == 2);
+        }
+
+        [Test]
+        public void ProcessLine_EmptyFile_ValidInput()
+        {
+            // Arrange
+            var objFile = new string[] {};
+
+            // Act
+            obj.LoadObj(objFile);
+
+            // Assert
+            Assert.IsTrue(obj.VertexList.Count == 0);
+        }
+        #endregion
+    }
+}
